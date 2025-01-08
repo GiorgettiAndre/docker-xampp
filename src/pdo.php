d7f4d862d175fbde9adf90b643496fc19abee6d5
st10575 <st10575@iismarconipieralisi.it>
2025-01-08 07:18:16 +0000

    pdo connection

--- src/pdo.php
+++ src/pdo.php
@@ -1,8 +1,7 @@
 <?php
 try{
-    $conn = new PDO("mysql:host=db;dbname=root_db;charset=utf8mb4", "user", "user");
+    $conn = new PDO("mysql:host=db;dbname=root_db;charset=utf8mb4", "user", "user"); 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "daje (PDO)";
 }
-catch (PDOException $e)
-{ die("so morto"); }
+catch (PDOException $e) { die("so morto"); }
