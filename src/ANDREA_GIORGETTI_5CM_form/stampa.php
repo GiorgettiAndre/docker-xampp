b6b971ddf337ddddb33e2432930dfae94d97324e
st10575 <st10575@iismarconipieralisi.it>
2025-01-15 07:47:53 +0000

    modifica stampe

--- src/ANDREA_GIORGETTI_5CM_form/stampa.php
+++ src/ANDREA_GIORGETTI_5CM_form/stampa.php
@@ -1,25 +1,31 @@
-<?php
-$dati_post = array();
-$dati_get = array();
-
-if(isset($_POST["password"]))
-    $_POST["password"] = md5($_POST["password"]); //eseguo l'hash della password 
-
-if(isset($_SERVER["REQUEST_METHOD"]))
-{
-    if($_SERVER["REQUEST_METHOD"] == "POST")
-        $dati_post = ["username", "email", "password", "nome", "cognome", "telefono", "residenza", "viaResidenza", "cap", "genere", "dataNascita", "luogoNascita"];
-    else if($_SERVER["REQUEST_METHOD"] == "GET")
-        $dati_get = ["search"];
-}
-?>
-
 <html>
     <head>
         <title>stampa infos</title>
     </head>
     <body>
         <?php
+        $dati_post = array();
+        $dati_get = array();
+
+        if(isset($_POST["password"]))
+            $_POST["password"] = md5($_POST["password"]); //eseguo l'hash della password 
+
+        if(isset($_SERVER["REQUEST_METHOD"]))
+        {
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                $dati_post = ["username", "email", "password", "nome", "cognome", "telefono", "residenza", "viaResidenza", "cap", "genere", "dataNascita", "luogoNascita"];
+                echo "<h2>Richiesta POST</h2>";
+            }
+            else if($_SERVER["REQUEST_METHOD"] == "GET")
+            {
+                $dati_get = ["search"];
+                echo "<h2>Richiesta GET</h2>";
+            }
+        }
+
+        echo "<br>";
+
         for($i = 0; $i < count($dati_post); $i++)
             echo $i+1 .') '. $_POST[$dati_post[$i]] . "<br>";
         
