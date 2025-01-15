831d10b1664172b9a6dafe7ff3e44fff6ad2c8e9
st10575 <st10575@iismarconipieralisi.it>
2025-01-15 08:33:21 +0000

    aggiorn. stampe metodi e introd. conn. db globale

--- src/ANDREA_GIORGETTI_5CM_form/stampa.php
+++ src/ANDREA_GIORGETTI_5CM_form/stampa.php
@@ -1,12 +1,24 @@
+<?php
+    function PrintMethod($method)
+    {
+        foreach($method as $key => $value)
+            echo $key .' => '. $method[$key] . "<br>";
+
+        echo "<br>";
+        echo "print_r:<br>";
+        print_r($method);
+        echo "<br>";
+        echo "var_dump:<br>";
+        var_dump($method);
+    }
+?>
+
 <html>
     <head>
         <title>stampa infos</title>
     </head>
     <body>
         <?php
-            $dati_post = [" "];
-            $dati_get = [" "];
-
             if(isset($_POST["password"]))
                 $_POST["password"] = md5($_POST["password"]); //eseguo l'hash della password 
 
@@ -14,31 +26,15 @@
             {
                 if($_SERVER["REQUEST_METHOD"] == "POST")
                 {
-                    $dati_post = ["username", "email", "password", "nome", "cognome", "telefono", "residenza", "viaResidenza", "cap", "genere", "dataNascita", "luogoNascita"];
                     echo "<h2>Richiesta POST</h2>";
+                    PrintMethod($_POST);
                 }
                 else if($_SERVER["REQUEST_METHOD"] == "GET")
                 {
-                    $dati_get = ["search"];
                     echo "<h2>Richiesta GET</h2>";
+                    PrintMethod($_GET);
                 }
             }
-
-            for($i = 0; $i < count($dati_post); $i++)
-                echo $i+1 .') '. $_POST[$dati_post[$i]] . "<br>";
-
-            echo "<br><br>";
-
-            for($i = 0; $i < count($dati_get); $i++)
-                echo $i+1 .') '. $_GET[$dati_get[$i]] . "<br>";
-
-            echo "print_r di post e get"; echo "<br>";
-            print_r($dati_post); echo "<br>";
-            print_r($dati_get); echo "<br>";
-            echo "<br>";echo "<br>";
-            echo "var_dump di post e get"; echo "<br>";
-            var_dump($dati_post); echo "<br>";
-            var_dump($dati_get); echo "<br>";
         ?>
     </body>
 </html>
--- src/mysqli.php
+++ src/mysqli.php
@@ -1,6 +1,6 @@
 <?php
 $conn = new mysqli("db", "user", "user", "root_db", 3306);
 if($conn->connect_error)
-    die("so morto");
-echo "daje (mysqli)";
-$conn->close();
+    die("so morto" . $conn->connect_error);
+/* echo "daje (mysqli)";
+$conn->close(); */
