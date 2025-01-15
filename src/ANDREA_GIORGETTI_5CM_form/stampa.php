dfd146c65219714c94f1f10a8d56fefb91e8df42
st10575 <st10575@iismarconipieralisi.it>
2025-01-15 07:49:34 +0000

    bla bla v2

--- src/ANDREA_GIORGETTI_5CM_form/stampa.php
+++ src/ANDREA_GIORGETTI_5CM_form/stampa.php
@@ -6,10 +6,10 @@
         <?php
             $dati_post = array();
             $dati_get = array();
-    
+
             if(isset($_POST["password"]))
                 $_POST["password"] = md5($_POST["password"]); //eseguo l'hash della password 
-    
+
             if(isset($_SERVER["REQUEST_METHOD"]))
             {
                 if($_SERVER["REQUEST_METHOD"] == "POST")
@@ -23,14 +23,12 @@
                     echo "<h2>Richiesta GET</h2>";
                 }
             }
-        
-            echo "<br>";
-        
+
             for($i = 0; $i < count($dati_post); $i++)
                 echo $i+1 .') '. $_POST[$dati_post[$i]] . "<br>";
-            
+
             echo "<br><br>";
-        
+
             for($i = 0; $i < count($dati_get); $i++)
                 echo $i+1 .') '. $_GET[$dati_get[$i]] . "<br>";
         ?>
